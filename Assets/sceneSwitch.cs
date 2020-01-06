a07620a61fda332ed83145c2401e7d1127e57e17
oxydy <dwhongwei@gmail.com>
2020-01-06 14:58:30 +0800

    First commit

---
+++ Assets/sceneSwitch.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneSwitch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void switch0()
+    {
+        SceneManager.LoadScene(sceneBuildIndex:0);
+    }
+    public void switch1()
+    {
+        SceneManager.LoadScene(sceneBuildIndex:1);
+    }
+    public void switch2()
+    {
+        SceneManager.LoadScene(sceneBuildIndex:2);
+    }
+    public void switch3()
+    {
+        SceneManager.LoadScene(sceneBuildIndex:3);
+    }
+    public void switch4()
+    {
+        SceneManager.LoadScene(sceneBuildIndex:4);
+ 
+   }
+
+    public void doExitGame()
+    {
+        Application.Quit();
+    }
+}
